b1a570a5d0047719574480e57c3b26fdae1832c8
Vaidhehi Muthuramalingam <138861743+Vaidhehi-Muthuramalingam@users.noreply.github.com>
2023-09-13 22:44:56 +0530

    Add files via upload
    Sir/Madam,
    
    Here i attached my class assignments 1 for evaluation. Kindly letting me know the corrections and feedback about class assignment.
    
    Thank You.

---
+++ multiplefunctions.py
@@ -0,0 +1,120 @@
+class multiplefunctions():
+    def Subfields():
+        lists=["Sub-fields in AI are:","Machine Learning","Neural Networks","Vision","Robotics","Speech Processing","Natural Language Processing"]
+        for subfields in lists:
+            print(subfields)
+            message=subfields
+            return message
+    def OddEven():
+        number=int(input("Enter a number:"))
+        if((number%2)==0):
+            print(number,"is Even number")
+            message=number,"is Even number"
+        else:
+            print(number,"is Odd number")
+            message=number,"is Odd number"
+        return message
+    def OddEven():
+        number=int(input("Enter a number:"))
+        if((number%2)==1):
+            print(number,"is Odd number")
+            message=number,"is Odd number"
+        else:
+            print(number,"is Even number")
+            message=number,"is Even number"
+        return message
+    def OddEven():
+        number=int(input("Enter a number:"))
+        if((number%2)==0):
+            print(number,"is Even number")
+            message=number,"is Even number"
+        elif((number%2)==1):
+            print(number,"is Odd number")
+            message=number,"is Odd number"
+        else:
+            print("Invalid")
+            message="Invalid"
+        return message
+    def Elegible():
+        your_gender=input("Your Gender:")
+        your_age=int(input("Your Age:"))
+        if(your_age==21):
+            print("ELIGIBLE")
+            message="ELIGIBLE"
+        else:
+            print("NOT ELIGIBLE")
+            message="NOT ELIGIBLE"
+        return message
+    def Elegible():
+        your_gender=input("Your Gender:")
+        your_age=int(input("Your Age:"))
+        if(your_age<21):
+            print("NOT ELIGIBLE")
+            message="NOT ELIGIBLE"
+        else:
+            print("ELIGIBLE")
+            message="ELIGIBLE"
+        return message
+    def percentageofmarks():
+        marks1=int(input("Enter the Subject1:"))
+        marks2=int(input("Enter the Subject2:"))
+        marks3=int(input("Enter the Subject3:"))
+        marks4=int(input("Enter the Subject4:"))
+        marks5=int(input("Enter the Subject5:"))
+        total=marks1+marks2+marks3+marks4+marks5
+        print("Total:",total)
+        markss=total
+        percentage=(total/500)*100
+        print("Percentage:",percentage)
+        markss=percentage
+        return markss
+    def percentageofmarks():
+        marks1=int(input("Enter the Subject1:"))
+        marks2=int(input("Enter the Subject2:"))
+        marks3=int(input("Enter the Subject3:"))
+        marks4=int(input("Enter the Subject4:"))
+        marks5=int(input("Enter the Subject5:"))
+        total=marks1+marks2+marks3+marks4+marks5
+        print("Total:",total)
+        markss=total
+        return markss
+        percentage=(total/500)*100
+        print("Percentage:",percentage)
+        markss=percentage
+        return markss
+    def totalofmarks(marks1,marks2,marks3,marks4,marks5):
+        totalofmarks=98+87+95+95+93
+        return totalofmarks
+    def percentageofmarks(marks1,marks2,marks3,marks4,marks5):
+        percentageofmarks=((98+87+95+95+93)/500)*100
+        return percentageofmarks
+    def areaoftriangle():
+        height=int(input("Enter the height of Triangle:"))
+        breadth=int(input("Enter the breadth of Triangle:"))
+        areaoftriangle=(height*breadth)/2
+        print("Area of Triangle:",areaoftriangle)
+        area=areaoftriangle
+        return area
+    def perimeteroftriangle():
+        height1=int(input("Enter the height1 of Triangle:"))
+        height2=int(input("Enter the height2 of Triangle:"))
+        breadthh=int(input("Enter the breadthh of Triangle:"))
+        perimeteroftriangle=height1+height2+breadthh
+        print("Perimeter of Triangle:",perimeteroftriangle)
+        perimeter=perimeteroftriangle
+        return perimeter
+    def areaoftriangle():
+        Height=int(input("Height:"))
+        Breadth=int(input("Breadth:"))
+        Area_formula=(Height*Breadth)/2
+        print("Area of Triangle:",Area_formula)
+        area=Area_formula
+        return area
+    def perimeteroftriangle():
+        Height1=int(input("Height1:"))
+        Height2=int(input("Height2:"))
+        Breadthh=int(input("Breadthh:"))
+        Perimeter_formula=Height1+Height2+Breadthh
+        print("Perimeter of Triangle:",Perimeter_formula)
+        perimeter=Perimeter_formula
+        return perimeter
